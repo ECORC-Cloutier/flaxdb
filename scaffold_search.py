fb458c8c7c1181d108a1da720683b1328cb1bc8e
AAFC <dhdouglas.huang@gmail.com>
2015-03-02 14:51:52 -0500

    add line wrapping

--- scaffold_search.py
+++ scaffold_search.py
@@ -1,5 +1,6 @@
 from Bio import SeqIO
 import sys
+import re
 
 file_name = sys.argv[1]
 ind = file_name.index(".")
@@ -11,17 +12,21 @@ existing_scaffolds = scaffold_list.readlines()
 file_read = open(file_name, "r")
 
 for seq_record in SeqIO.parse(file_read, "fasta"):
-    curr_id = seq_record.id
-    curr_seq = seq_record.seq
+    curr_id = str(seq_record.id)
+    curr_seq = str(seq_record.seq)
     if curr_id not in existing_scaffolds:
+        write_seq = re.sub("(.{70})", "\\1\n", curr_seq, 0, re.DOTALL)
         fasta_write = open(curr_id+".fasta", "w")
-        fasta_write.write(">"+str(base)+"\n"+str(curr_seq)+"\n")
+        print("New: "+curr_id+"\n")
+        fasta_write.write(">"+str(base)+"\n"+str(write_seq)+"\n")
         fasta_write.close()
         existing_scaffolds.append(curr_id)
         scaffold_list.write(curr_id+"\n")
     else:
+        write_seq = re.sub("(.{70})", "\\1\n", curr_seq, 0, re.DOTALL)
         fasta_write = open(curr_id+".fasta", "a")
-        fasta_write.write(">"+str(base)+"\n"+str(curr_seq)+"\n")
+        fasta_write.write(">"+str(base)+"\n"+str(write_seq)+"\n")
+        print("Old: "+curr_id+"\n")
         fasta_write.close()
 
 file_read.close()
