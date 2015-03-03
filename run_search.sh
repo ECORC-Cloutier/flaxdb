9038c0b99f649f4c01209d0d0c7d34383d0e32a0
AAFC <dhdouglas.huang@gmail.com>
2015-03-03 11:53:55 -0500

    changes

--- run_search.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for file in *consensus.fasta
-do
-    python scaffold_search.py $file
-done
--- scaffold_search.py
+++ scaffold_search.py
@@ -6,28 +6,14 @@ file_name = sys.argv[1]
 ind = file_name.index(".")
 base = file_name[:ind]
 
-scaffold_list = open("scaffold_list.txt", "a+")
-existing_scaffolds = scaffold_list.readlines() 
-
 file_read = open(file_name, "r")
 
 for seq_record in SeqIO.parse(file_read, "fasta"):
     curr_id = str(seq_record.id)
     curr_seq = str(seq_record.seq)
-    if curr_id not in existing_scaffolds:
-        write_seq = re.sub("(.{70})", "\\1\n", curr_seq, 0, re.DOTALL)
-        fasta_write = open(curr_id+".fasta", "w")
-        print("New: "+curr_id+"\n")
-        fasta_write.write(">"+str(base)+"\n"+str(write_seq)+"\n")
-        fasta_write.close()
-        existing_scaffolds.append(curr_id)
-        scaffold_list.write(curr_id+"\n")
-    else:
-        write_seq = re.sub("(.{70})", "\\1\n", curr_seq, 0, re.DOTALL)
-        fasta_write = open(curr_id+".fasta", "a")
-        fasta_write.write(">"+str(base)+"\n"+str(write_seq)+"\n")
-        print("Old: "+curr_id+"\n")
-        fasta_write.close()
+    write_seq = re.sub("(.{70})", "\\1\n", curr_seq, 0, re.DOTALL)
+    fasta_write = open(curr_id+".fasta", "a")
+    fasta_write.write(">"+str(base)+"\n"+str(write_seq)+"\n")
+    fasta_write.close()
 
 file_read.close()
-scaffold_list.close()
