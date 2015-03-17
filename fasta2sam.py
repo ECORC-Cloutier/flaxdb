99d9312017d30746362a2d92fde45107eb960adc
AAFC <dhdouglas.huang@gmail.com>
2015-03-17 12:04:13 -0400

    fix gapped reads

--- fasta2sam.py
+++ fasta2sam.py
@@ -8,22 +8,22 @@ base = name[:ind]
 file_read = open(name,"r")
 file_write = open(base+".sam","w")
 
-file_write.write("@SQ SN:"+base+" LN:unknown"+"\n")
-
-last_index = 0
-gap_count = 0
+file_write.write("@SQ\tSN:"+base+"\tLN:unknown\n")
 
 for record in SeqIO.parse(file_read,"fasta"):
     raw_seq = str(record.seq).split("N")
     qname = record.id
+    gap_count = 0
+    last_end = 0
     for item in raw_seq:
         if len(item) > 0:
-            end = last_index+gap_count+len(item)
-            file_write.write(qname+"\t"+"*"+"\t"+base+"\t"+str(last_index+1)+"\t"+"255"+"\t"+"*"+"\t"+"="+"\t"+str(end)+"\t"+"0"+"\t"+item+"\t"+"*"+"\n")
+            start = gap_count + last_end + 1
+            end = start + len(item)
+            file_write.write(qname+"\t"+"3"+"\t"+base+"\t"+str(start)+"\t"+"255"+"\t"+str(len(item))+"M"+"\t"+"="+"\t"+str(end)+"\t"+"0"+"\t"+item+"\t"+"*"+"\n")
+            last_end = end
             gap_count = 0
-            last_index = end
         else:
-            gap_count+=1
-            
+            gap_count += 1
+
 file_read.close()
 file_write.close()
