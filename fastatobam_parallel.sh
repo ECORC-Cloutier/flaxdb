f4e925053323953613ed9dc1ac83c5ecbe628ab3
AAFC <dhdouglas.huang@gmail.com>
2015-03-19 14:44:44 -0400

    script to run with GNU parallel

---
+++ fastatobam_parallel.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+fastatobam() 
+{
+    file=$1
+    base=${file%%.*}
+    python fasta2sam_single.py $file
+    samtools view -bS ${base}.sam > ${base}.bam
+    samtools sort ${base}.bam ${base}_sorted
+    #samtools index ${base}_sorted.bam
+}
+
+export -f fastatobam
+
+parallel fastatobam {} ::: *.fasta
+
+counter=0
+foldernum=1
+
+mkdir ~/flaxdb-data/consensus_1
+dir=~/flaxdb-data/consensus_1
+
+for file in *sorted.bam
+do
+    counter=$((counter+1))
+    if [ $counter -lt 1000 ] 
+    then
+        mv $file $dir
+    else
+        if [ `expr $counter % 1000` -eq 0 ]
+        then
+            foldernum=$((foldernum+1))
+            mkdir ~/flaxdb-data/consensus_${foldernum}
+            dir=~/flaxdb-data/consensus_${foldernum}
+            mv $file $dir
+        else
+            mv $file $dir
+        fi
+    fi
+done      
