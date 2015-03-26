ca0a50d465e7895cbc08646a95cc0177f7e4537d
AAFC <dhdouglas.huang@gmail.com>
2015-03-26 13:29:13 -0400

    add comments

--- rename.sh
+++ rename.sh
@@ -1,6 +1,7 @@
-!/bin/bash
+#When placed into the directory of .fasta files and given a .csv files of properly named genotypes, this script will replace the corresponding file names with the proper name.
+#!/bin/bash
 
-for file in *.fasta
+for file in *.fasta #remove extraneous words from genotype file name
 do
     newname=${file%%.*}.fasta
     mv $file $newname
@@ -12,6 +13,6 @@ do
     replace=${col2}.fasta
     echo $query
     echo $replace
-    find -name ${query} -exec mv {} $replace \;
-done < flax_rename.csv
+    find -name ${query} -exec mv {} $replace \; #matching the genotype file name with the proper name
+done < flax_rename.csv #.csv containing proper names
     
