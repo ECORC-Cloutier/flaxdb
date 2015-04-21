577bbfd96715797755aa51bb0763cc0021b3551a
AAFC <dhdouglas.huang@gmail.com>
2015-04-21 14:43:56 -0400

    add gapped bam tool

---
+++ gapped_bam/README.md
@@ -0,0 +1,9 @@
+# Gapped BAM
+
+This shell script outputs a list (both in .csv and .fasta) of all reference sequences that corresponds to gaps in an alignment. It requires BedTools.
+
+To run the script:
+
+    bash findgaps.sh input.bam reference_genome.fasta
+
+where input.bam is your input .bam file and reference_genome.fasta is your reference genome. The .bam file MUST be sorted and its headers MUST be identical to those of the reference .fasta file.
---
+++ gapped_bam/findgaps.sh
@@ -0,0 +1,14 @@
+#Author: Douglas Huang
+#Modified: April 21, 2015
+#Function: Get a list of all the reference genome sequences corresponding to gaps in the alignment
+#Instructions: Use the input .bam file as the first command-line argument and the reference genome .fasta file as the second argument
+
+#!/bin/bash
+
+write_name=${1%%.*}
+ref_genome=$2
+
+bedtools genomecov -ibam $1 -bga > ${write_name}_coverage_coordinates.bedgraph #generates a list of read coverage per position for entire genome
+awk '$4 < 1 {print $1 "\t" $2 "\t" $3}' ${write_name}_coverage_coordinates.bedgraph > ${write_name}_gap_coordinates.bed #filters out all entries greater than 1 (to find only gaps) and converts to .bed file
+bedtools getfasta -tab -fi $2 -bed ${write_name}_gap_coordinates.bed -fo ${write_name}_gap_sequences.csv #creates .csv file of all gapped sequences
+bedtools getfasta -fi $2 -bed ${write_name}_gap_coordinates.bed -fo ${write_name}_gap_sequences.fasta #creates .fasta file of all gapped sequences
