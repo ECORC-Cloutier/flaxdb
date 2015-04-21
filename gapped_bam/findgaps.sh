6677a11236472a15fb490d8291383f5a177763fc
AAFC <dhdouglas.huang@gmail.com>
2015-04-21 15:04:45 -0400

    change output to .csv

--- gapped_bam/findgaps.sh
+++ gapped_bam/findgaps.sh
@@ -8,7 +8,7 @@
 write_name=${1%%.*}
 ref_genome=$2
 
-bedtools genomecov -ibam $1 -bga > ${write_name}_coverage_coordinates.bedgraph #generates a list of read coverage per position for entire genome
-awk '$4 < 1 {print $1 "\t" $2 "\t" $3}' ${write_name}_coverage_coordinates.bedgraph > ${write_name}_gap_coordinates.bed #filters out all entries greater than 1 (to find only gaps) and converts to .bed file
-bedtools getfasta -tab -fi $2 -bed ${write_name}_gap_coordinates.bed -fo ${write_name}_gap_sequences.csv #creates .csv file of all gapped sequences
-bedtools getfasta -fi $2 -bed ${write_name}_gap_coordinates.bed -fo ${write_name}_gap_sequences.fasta #creates .fasta file of all gapped sequences
+bedtools genomecov -ibam $1 -bga > ${write_name}_coverage_coordinates.csv #generates a list of read coverage per position for entire genome
+awk '$4 < 1 {print $1 "\t" $2 "\t" $3}' ${write_name}_coverage_coordinates.csv > ${write_name}_gap_coordinates.csv #filters out all entries greater than 1 (to find only gaps) and converts to .bed file
+bedtools getfasta -tab -fi $2 -bed ${write_name}_gap_coordinates.csv -fo ${write_name}_gap_sequences.csv #creates .csv file of all gapped sequences
+bedtools getfasta -fi $2 -bed ${write_name}_gap_coordinates.csv -fo ${write_name}_gap_sequences.fasta #creates .fasta file of all gapped sequences
